a3550e545cce1549fb0a51cb93c113f9345a9573
Haadi <lonehaadi81@gmail.com>
2026-06-03 16:41:53 +0530

    a

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,28 @@ const nextConfig: NextConfig = {
     // your project has type errors.
     ignoreBuildErrors: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**', 
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**', 
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**', 
+      },
+    ],
+  },
 };
 
 export default nextConfig;
